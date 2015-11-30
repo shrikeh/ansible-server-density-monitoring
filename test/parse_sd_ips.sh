e50d525ba0406f33f8b57fa8a2698142d723828a
Barney Hanlon <barney@shrikeh.net>
2015-11-30 13:06:24 +0000

    Initial commit

---
+++ test/parse_sd_ips.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+function sd_parse_urls {
+  while [[ ${#} > 1 ]]
+  do
+    local key="${1}";
+    case ${key} in
+        -f|--file)
+        local SD_TEMP_FILE="${2}";
+        shift # past argument
+        ;;
+        *)
+                # unknown option
+        ;;
+    esac
+    shift # past argument or value
+  done
+  cat "${SD_TEMP_FILE}" | grep '"ip">' | awk 'BEGIN { FS = ">" }; { print $2 }' | awk 'BEGIN { FS = "<" }; { print $1 }'
+  return;
+}
+
+sd_parse_urls "$@";
